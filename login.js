09150da6dbe7d0a240dcdf3914b830533490bd33
Phanurat <paunavt5@gmail.com>
2024-04-24 15:56:39 +0700

    cut word

--- login.js
+++ login.js
@@ -1,30 +1,57 @@
 const puppeteer = require('puppeteer');
+const { minify } = require('minify-html');
+const fs = require('fs');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.facebook.com/login');
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.facebook.com/login');
 
-    // รอให้หน้าเว็บโหลดเสร็จ
-    await page.waitForSelector('input[name="email"]');
+        // รอให้หน้าเว็บโหลดเสร็จ
+        await page.waitForSelector('input[name="email"]');
 
-    // กรอกชื่อผู้ใช้หรืออีเมล
-    await page.type('input[name="email"]', 'paunavt55@gmail.com');
+        // กรอกชื่อผู้ใช้หรืออีเมล
+        await page.type('input[name="email"]', 'paunavt55@gmail.com');
 
-    // กรอกรหัสผ่าน
-    await page.type('input[name="pass"]', 'p2p4te45');
+        // กรอกรหัสผ่าน
+        await page.type('input[name="pass"]', 'p2p4te45');
 
-    // กดปุ่มเข้าสู่ระบบ
-    await page.click('button[type="submit"]');
+        // กดปุ่มเข้าสู่ระบบ
+        await page.click('button[type="submit"]');
 
-    // รอให้หน้าเว็บโหลดหลังจากเข้าสู่ระบบ
-    await page.waitForNavigation();
+        // รอให้หน้าเว็บโหลดหลังจากเข้าสู่ระบบ
+        await page.waitForNavigation();
 
-    // เข้าสู่ระบบสำเร็จแล้ว ตอนนี้คุณสามารถดำเนินการต่อได้ตามต้องการ
-    console.log('Logged in successfully');
+        // เข้าสู่ระบบสำเร็จแล้ว ตอนนี้คุณสามารถดำเนินการต่อได้ตามต้องการ
+        console.log('Logged in successfully');
 
-    // ปิดเบราว์เซอร์
-    await browser.close();
+        // Close the login page since we no longer need it
+        await page.close();
+
+        const page_link = await browser.newPage();
+        await page_link.goto('https://www.facebook.com/people/Gehenna-Gate/61558499640631/');
+    
+        //add content HTML
+        const htmlContent = await page_link.content();
+
+        // Close the page after extracting content
+        await page_link.close();
+
+        await browser.close();
+
+        const minifiedHTML = await minify(htmlContent, {
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyCSS: true,
+            minifyJS: true
+        });
+
+        fs.writeFileSync('scrap/test.html', minifiedHTML);
+
+        console.log('Webpage saved as file to Folder!!');
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 })();
-
-module.exports = { loginToFacebook };
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from bs4 import BeautifulSoup
 from pythainlp.tokenize import word_tokenize
 
 word_cut = ['เข้าสู่ระบบ', 'ลืมบัญชีใช่ไหม', '·', 'แชร์กับ', 'สาธารณะ', 'สร้างบัญชีใหม่', 'เข้าสู่ระบบหรือสมัครใช้งาน',
-            'เพื่อเชื่อมต่อกับเพื่อน', 'เกี่ยวกับ', 'รูปภาพ', 'วิดีโอ', 'แนะนำตัว']
+            'เพื่อเชื่อมต่อกับเพื่อน', 'เกี่ยวกับ', 'รูปภาพ', 'วิดีโอ', 'แนะนำตัว', 'Log in', 'Forgotten account?' ,'See more on Facebook', 'Email address or phone number',
+            'Forgotten password?', 'Create New Account', 'About', 'Photos', 'Videos', 'More', 'Intro', 'Page  Writer  Photographer',
+            'Bangkok, Thailand, Bangkok', '095 659 5973', 'paunavt55@gmail.com', 'Photos See All Photos', 'See All Photos', 'Privacy   Terms   Advertising   Ad choices    Cookies   More   Meta © 2024',
+            'Password', 'or', 'View more comments', 'Privacy', 'Terms', 'Advertising', 'Ad choices', 'Cookies', 'Meta © 2024', 'Page', 'Writer',
+            'Photographer', 'yet', 'rated', '(0 reviews)','Ad','choices', 'Meta', '©']
 
 # เปิดไฟล์ HTML
 with open('scrap/test1.html', 'r', encoding='utf-8') as file:
@@ -15,7 +19,7 @@ soup = BeautifulSoup(html_content, 'html.parser')
 printed_text = set()
 
 # เปิดไฟล์ output.txt ในโหมดการเขียน ('w') เพื่อเตรียมเขียนข้อมูล
-with open('output/output1.txt', 'w', encoding='utf-8') as f:
+with open('output/output.txt', 'w', encoding='utf-8') as f:
     # ค้นหาและตัดคำในแท็ก <span>
     for paragraph in soup.find_all('span'):
         # ตรวจสอบว่าข้อความภายในแต่ละ <span> ไม่ซ้ำกับข้อความที่มีอยู่แล้ว และไม่ซ้ำกับ word_cut
