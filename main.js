eee299648a0f9557e6c86a83efa1111d6cf2e481
Phanurat <paunavt5@gmail.com>
2024-04-24 14:04:53 +0700

    delete pop up facebook login

--- main.js
+++ main.js
@@ -4,26 +4,29 @@ const fs = require('fs');
 
 (async () => {
     try {
-        // เปิดเบราว์เซอร์และสร้างหน้าใหม่โดยใช้โปรไฟล์ที่ 2 ของ Google Chrome
         const browser = await puppeteer.launch({
             userDataDir: 'C:\\Users\\Mr.D\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2',
-            args: ['--no-sandbox', '--disable-setuid-sandbox'] // ใช้งาน Puppeteer ในระบบปฏิบัติการ Windows
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
 
-        // เข้าสู่ URL ของเว็บไซต์ที่ต้องการบันทึก
-        await page.goto('https://www.facebook.com/permalink.php?story_fbid=pfbid02NuTWEkU2efbMa1EATFLphX2umLM2h9rbdCg1wCFgYWAojJL7Ue9ZZx5Z2WXyBfSZl&id=61558499640631');
+        await page.goto('https://www.facebook.com/profile.php?id=61558499640631');
 
-        // รอให้หน้าเว็บโหลดเสร็จ
         await page.waitForSelector('body');
 
-        // ดึงเนื้อหา HTML ของหน้าเว็บ
+        // Evaluate script in the context of the page to remove the popup
+        await page.evaluate(() => {
+            // Replace the selector with the actual selector for the popup
+            const popupElement = document.querySelector('your-popup-selector');
+            if (popupElement) {
+                popupElement.remove();
+            }
+        });
+
         const htmlContent = await page.content();
 
-        // ปิดเบราว์เซอร์
         await browser.close();
 
-        // บีบอัดและ minify HTML content
         const minifiedHTML = await minify(htmlContent, {
             collapseWhitespace: true,
             conservativeCollapse: true,
@@ -31,8 +34,7 @@ const fs = require('fs');
             minifyJS: true
         });
 
-        // บันทึกไฟล์ HTML ไปยังเครื่องและเขียนลงในไฟล์
-        fs.writeFileSync('scrap/test1.html', minifiedHTML);
+        fs.writeFileSync('scrap/test2.html', minifiedHTML);
 
         console.log('Webpage saved as compressed_webpage.html');
     } catch (error) {
