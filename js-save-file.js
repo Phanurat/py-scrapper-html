477a673eda084435ae7a21a378a2d52c3580639d
Phanurat <paunavt5@gmail.com>
2024-04-27 17:47:49 +0700

    edit

--- js-save-file.js
+++ js-save-file.js
@@ -2,28 +2,27 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     try {
-        const browser = await puppeteer.launch({ headless: false }); // เปิดเบราว์เซอร์ในโหมดแสดงผล
+        const browser = await puppeteer.launch({ headless: false }); 
         const page = await browser.newPage();
 
         await page.goto('https://www.facebook.com/aseanfootball', { waitUntil: 'networkidle2' });
-
-        //await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-        // คลิกที่ปุ่ม "Close"
         await page.click('[aria-label="Close"]');
+        
+        // ใช้เมธอด page.waitForTimeout() ใน Puppeteer แทน
+        await page.waitForTimeout(3000); 
 
-        // รอให้หน้าเว็บโหลดสมบูรณ์
-        await page.waitForTimeout(3000); // เพิ่มระยะเวลาในการรอตามที่เหมาะสม
-
-        // ดึง HTML หลังจากไปที่ลิ้งนี้
         const htmlContent = await page.content();
 
-        // เขียน HTML ลงในไฟล์
-        const fs = require('fs');
-        fs.writeFileSync('scrap/facebook_after_login.html', htmlContent);
-
-        // ปิดเบราว์เซอร์
         await browser.close();
+
+        const minifiedHTML = await minify(htmlContent, {
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyCSS: true,
+            minifyJS: true
+        });
+
+        fs.writeFileSync('scrap/test_cut.html', minifiedHTML);;
 
         console.log('Facebook page after login saved as facebook_after_login.html');
     } catch (error) {
