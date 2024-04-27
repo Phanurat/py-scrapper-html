0ed4ed8106e58db9d6399a0beb85894f29425b86
Phanurat <paunavt5@gmail.com>
2024-04-27 13:10:25 +0700

    add js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const fs = require('fs');
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        await page.goto('https://www.facebook.com/aseanfootball');
+        await page.goto('https://www.facebook.com/hashtag/%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B8%81');
 
         // รอให้ element ปรากฏ
         await page.waitForSelector('.__fb-light-mode.x1n2onr6.xzkaem6');
