a930a9acd4474c3978223ab53068cda2a1de9222
Phanurat <paunavt5@gmail.com>
2024-04-27 13:18:41 +0700

    button close finishing!!

--- js-save-file.js
+++ js-save-file.js
@@ -9,19 +9,26 @@ const puppeteer = require('puppeteer');
 
         //await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-        // รอให้ element ปรากฏ
-        await page.waitForSelector('.__fb-light-mode.x1n2onr6.xzkaem6');
+        // คลิกที่ปุ่ม "Close"
+        await page.click('[aria-label="Close"]');
+
+        // รอให้หน้าเว็บโหลดสมบูรณ์
+        await page.waitForTimeout(3000); // เพิ่มระยะเวลาในการรอตามที่เหมาะสม
+
+        // ดึง HTML หลังจากคลิกปุ่ม "Close"
+        //const htmlContent = await page.content();
+
 
         // ลบ element โดยใช้ evaluate ซึ่งจะทำให้ JavaScript ทำงานใน context ของหน้าเว็บ
-        await page.evaluate(() => {
+        /*await page.evaluate(() => {
             // เลือก element ที่ต้องการลบ แล้วลบออกไป
             const elementToRemove = document.querySelector('.__fb-light-mode.x1n2onr6.xzkaem6');
             if (elementToRemove) {
                 elementToRemove.remove();
             }
-        });
+        });*/
 
-        console.log('Element removed successfully');
+        //console.log('Close Button Successfully');
 
         // รอให้หน้าเว็บโหลดสมบูรณ์
         //await page.waitForNavigation({ waitUntil: 'networkidle2' });
