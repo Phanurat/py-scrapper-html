a2500c3c0fdaeee58c779175fd8313be3ae8a9e7
Phanurat <paunavt5@gmail.com>
2024-05-02 14:00:27 +0700

    Add GUI add link

--- py-gui.py
+++ py-gui.py
@@ -2,31 +2,40 @@ import tkinter as tk
 
 def get_link_page():
     link_page = entry.get()
-    label_result.config(text="URL: " + link_page)
-
-def get_name_page():
-    #name_page = entry.get()
-    name_page_res.config(text="Page Name:" + name_page)
+    link_result.config(text="URL: Pages FB" + link_page)
 
 # สร้างหน้าต่างหลัก
 root = tk.Tk()
 root.title("FB Scrapper")
 
 # สร้าง Label แสดงข้อความ
-label = tk.Label(root, text="ยินดีต้อนรับสู่โปรแกรม GUI ง่ายๆ", padx=200, pady=100)
+label = tk.Label(root, text="Facebook Scrapper News", padx=200, pady=20)
 label.pack()
 
-# สร้างช่อง input
+# สร้าง Label แสดงคำแนะนำสำหรับช่อง input สำหรับคำที่ต้องการสร้าง keyword
+title_label = tk.Label(root, text="Link:", padx=20, pady=10)
+title_label.pack()
+
+# สร้างช่อง input สำหรับ URL และกำหนด placeholder
 entry = tk.Entry(root)
+entry.insert(0, "วาง URL")  # 0 คือตำแหน่งเริ่มต้น
 entry.pack()
 
-# สร้างปุ่ม
-button = tk.Button(root, text="Scrapper", command=get_link_page)
-button.pack()
+# Label แสดงผลลัพธ์ของ URL
+link_result = tk.Label(root, text="", padx=20, pady=10)
+link_result.pack()
 
-# Label แสดงผลลัพธ์
-label_result = tk.Label(root, text="", padx=20, pady=10)
-label_result.pack()
+# สร้างปุ่มเพื่อดึงข้อมูลจากช่อง input สำหรับ keyword
+button_word_group = tk.Button(root, text="Link Page", command=get_link_page)
+button_word_group.pack()
+
+# Label แสดงผลลัพธ์ของ URL
+link_result = tk.Label(root, text="", padx=20, pady=10)
+link_result.pack()
+
+# Label แสดงชื่อเพจ
+name_page_res = tk.Label(root, text="", padx=20, pady=10)
+name_page_res.pack()
 
 # เริ่มการทำงานของ GUI
 root.mainloop()
