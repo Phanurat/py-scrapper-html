925bf6204ce2e60e814eb523bb8c267c37005d5a
Phanurat <paunavt5@gmail.com>
2024-05-04 13:46:14 +0700

    add save data to json file

--- py-save-json.py
+++ py-save-json.py
@@ -1,17 +1,31 @@
 import json
 
-# สร้างข้อมูลที่จะเก็บเป็น JSON
+# กำหนดที่อยู่ของไฟล์ที่มีลิงก์ link.txt
+file_path = "link_cut/link.txt"
+    
+# อ่านข้อมูลจากไฟล์ link.txt
+with open(file_path, "r") as link_file:
+    # อ่านและแยกลิงก์ตามบรรทัด
+    links = link_file.readlines()
+
+page_name = "Test pagename"
+title_name = "Test title name"
+content_news = "🇲🇾 Football Association of Malaysia (FAM) has appointed Spanish 🇪🇸 coach Javier Jorda Ribera as head coach of U17 Malaysia.Javier, who is also the Director of Johor Darul Ta'zim Football Academy, will lead the Malaysia U17 team to participate in the 2024 AFF U-16 Championship in Jakarta, Indonesia and the 2025 AFC U-17 Asian Cup Qualifiers.FAM"
+
+# สร้าง dictionary ที่มี key เป็น "links" และมีค่าเป็นรายการลิงก์
 data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
+    "pagename": {
+        "title": title_name,
+        "content": content_news,
+        "url": links
+    }
 }
 
 # กำหนดที่อยู่ของไฟล์ JSON ที่ต้องการจะเขียน
-file_path = "json_news/data.json"
+json_file_path = "json_news/links.json"
 
 # เขียนข้อมูลลงในไฟล์ JSON
-with open(file_path, "w") as json_file:
+with open(json_file_path, "w") as json_file:
     json.dump(data, json_file)
 
-print("เก็บข้อมูลเป็น JSON สำเร็จแล้ว!")
+print("เก็บลิงก์เป็น JSON สำเร็จแล้ว!")
