8e27535633ddcc4735bdacc9cdb1ef6fa9956ff8
Phanurat <paunavt5@gmail.com>
2024-05-04 11:56:27 +0700

    get many a lot of news for 1 min

--- js-save-file.js
+++ js-save-file.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const { minify } = require('html-minifier'); // เพิ่มการนำเข้า html-minifier
+const { minify } = require('html-minifier');
 const fs = require('fs');
 
 (async () => {
@@ -10,22 +10,22 @@ const fs = require('fs');
         await page.goto('https://www.facebook.com/aseanfootball', { waitUntil: 'networkidle2' });
         await page.click('[aria-label="Close"]');
         
-        // รอให้เห็นส่วนของเนื้อหาที่ต้องการ
         await page.waitForSelector('body');
 
-        const htmlContent = await page.content();
+        setTimeout(async () => {
+            const htmlContent = await page.content();
+            const minifiedHTML = minify(htmlContent, {
+                collapseWhitespace: true,
+                conservativeCollapse: true,
+                minifyCSS: true,
+                minifyJS: true
+            });
 
-        const minifiedHTML = minify(htmlContent, { // ใช้ฟังก์ชัน minify จาก html-minifier
-            collapseWhitespace: true,   
-            conservativeCollapse: true,
-            minifyCSS: true,
-            minifyJS: true
-        });
+            fs.writeFileSync('scrap/test_cut.html', minifiedHTML);
+            await browser.close();
 
-        fs.writeFileSync('scrap/test_cut.html', minifiedHTML);
-        await browser.close();
-
-        console.log('Facebook page after login saved as facebook_after_login.html');
+            console.log('Facebook page after login saved as facebook_after_login.html');
+        }, 60000); // รอ 1 นาที (60,000 มิลลิวินาที) ก่อนที่จะบันทึกไฟล์
     } catch (error) {
         console.error('Error:', error);
     }
